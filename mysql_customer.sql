ace4aca3bf7b1ca6583a6c8bbcd6033a9655fd51
weinsheim <txn@mail.de>
2019-04-21 04:20:04 +0800

    Create mysql_customer.sql

---
+++ mysql_customer.sql
@@ -0,0 +1,3 @@
+INSERT INTO `paros_customer_org` (`CustomerID`, `UserID`, `Remark`) VALUES (25104, 0, '陕西省侨联');		
+INSERT INTO `paros_customer_org` (`CustomerID`, `UserID`, `Remark`) VALUES (26304, 0, '上海发展研究基金会会员\nbashusong@drc.gov.cn');
+INSERT INTO `paros_customer_org` (`CustomerID`, `UserID`, `Remark`) VALUES (26305, 0, 'BaekChunhwa@baekc.com\nh.saori2010@gmail.com\n浦东新区欧美同学会会员\n欧美同学会浦东分会');
