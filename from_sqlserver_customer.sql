4a2248af03dcd2f20763573f0e5cf04aa3475676
weinsheim <txn@mail.de>
2019-04-21 04:17:02 +0800

    Create from_sqlserver_customer.sql

---
+++ from_sqlserver_customer.sql
@@ -0,0 +1,10 @@
+INSERT INTO `paros_customer_org` (`CustomerID`, `UserID`, `Remark`) VALUES (25104, 0, '陕西省侨联');
+INSERT INTO `paros_customer_org` (`CustomerID`, `UserID`, `Remark`) VALUES (26304, 0, '上海发展研究基金会会员 bashusong@drc.gov.cn');
+GO
+print 'Processed 100 total records'
+INSERT INTO `paros_customer_org` (`CustomerID`, `UserID`, `Remark`) VALUES (26305, 0, 'BaekChunhwa@baekc.com
+h.saori2010@gmail.com
+浦东新区欧美同学会会员
+欧美同学会浦东分会');
+GO
+print 'Processed 100 total records'
